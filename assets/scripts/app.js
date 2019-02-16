265ef901aea7d0b03f632d9b1d0f3ce9715a6b68
carlojacob <carlo.jacob@gmail.com>
2019-02-16 15:35:40 -0500

    Create app and api files/folders
    - Create `event`, `api` and `ui` files for auth, order, cart & product.
    - Add call to event handlers function in `app.js`.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,13 @@
 'use strict'
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
+const authenticationEvents = require('./auth/events')
+const productEvents = require('./product/events')
+const cartEvents = require('./cart/events')
+const orderEvents = require('./order/events')
 
 $(() => {
-  // your JS code goes here
+  authenticationEvents.addAuthEventHandlers(event)
+  productEvents.addAuthEventHandlers(event)
+  cartEvents.addAuthEventHandlers(event)
+  orderEvents.addAuthEventHandlers(event)
 })
