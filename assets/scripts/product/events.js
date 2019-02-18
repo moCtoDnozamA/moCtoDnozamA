954af720a85013c8e3fc4804ccf6d0a3177bed72
carlojacob <carlo.jacob@gmail.com>
2019-02-17 19:45:41 -0500

    Update API requests to index and show products
    - Modify events, ui and handlebars to correctly target product ids.
    - Refer to `onAddCart` callback when "Add to Cart" button is clicked.
    - Add comments to files.

--- assets/scripts/product/events.js
+++ assets/scripts/product/events.js
@@ -3,6 +3,8 @@
 const api = require('./api')
 const ui = require('./ui')
 
+const cartEvents = require('../cart/events')
+
 const onGetProducts = event => {
   event.preventDefault()
 
@@ -15,7 +17,8 @@ const onGetProducts = event => {
 
 const onGetProduct = event => {
   event.preventDefault()
-  const productId = $(event.target).closest('div').data('id')
+  // Store product data-id from handlebars file
+  const productId = $(event.target).closest('div.product').data('id')
 
   api.getProduct(productId)
     .then(ui.onGetProductSuccess)
@@ -38,8 +41,16 @@ const addProductEventHandlers = event => {
   // TODO: Only used to initially seed `products` collection
   // onSeedProducts()
   // End TODO: Only used to initially seed `products` collection
+  // For click on "Home" button
   $('#index-products').on('click', onGetProducts)
+  // For click on View Product button - not add to cart button
+  // TODO: Probably remove this after ui is set up.
   $('#show-product').on('click', onGetProduct)
+  // For click on "Add to Cart" button, refers to `cart/events`.
+  // TODO: Is this already included in handlers in `cart/events`?
+  $('#products').on('click', '.add-cart', cartEvents.onAddCart)
+  // For click on product div - not "Add to Cart" button
+  $('#products').on('click', '.show-product', onGetProduct)
 }
 
 module.exports = {
--- assets/scripts/product/ui.js
+++ assets/scripts/product/ui.js
@@ -3,12 +3,27 @@
 const showProductsTemplate = require('../templates/product-listing.handlebars')
 
 const onGetProductsSuccess = productsData => {
+  // Handlebars cannot access `_id` in each product object, therefore add field
+  // `id` = `_id` to each product object
+  productsData.products.forEach(product => { product.id = product['_id'] })
+  // send productsData to handlebars file to build index data
+  // TODO: Add check whether user is logged in to "if" statement in handlebars.
   const showProductsHTML = showProductsTemplate({ products: productsData.products })
+  // add indexed products to div for output to user
   $('#products').html(showProductsHTML)
 }
 
-const onGetProductSuccess = () => {
-
+const onGetProductSuccess = productData => {
+  // Handlebars cannot access `_id` in the product object, therefore add field
+  // `id` = `_id` to the product object
+  productData.product.id = productData.product['_id']
+  // Handlebars iterates over arrays, therefore put single product into array.
+  // TODO: may not use this handelbars file to populate div for output
+  const productArray = [productData.product]
+  // send productData to handlebars file to build show data
+  const showProductsHTML = showProductsTemplate({ products: productArray })
+  // add showed product to div for output to user
+  $('#products').html(showProductsHTML)
 }
 
 module.exports = {
