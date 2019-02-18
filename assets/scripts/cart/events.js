fcd9bcf16a707df245807c7a6d6097f80ff17dc6
xiaojie zhang <xiaojiezhang@xiaojiedeMacBook-Air.local>
2019-02-18 14:19:13 -0500

    Add cart -- when user sign up ,will create a cart depend on user id -- when user sign in ,will get a cart search user in in the api -- when the user add product in cart, it will show the product detail in cart -- in cart there are add and sub button to add or sub,when the array is empty, the div will be empty -- in ui function ,will calculation the total price

--- assets/scripts/cart/events.js
+++ assets/scripts/cart/events.js
@@ -2,11 +2,96 @@
 
 const api = require('./api')
 const ui = require('./ui')
+const store = require('../store')
+
+// const onGetCarts = event => {
+//   console.log(store.user._id)
+//   api.getCarts()
+//     .then(console.log)
+//     .catch(console.error)
+// }
+
+const onGetCart = () => {
+  api.getCart(store.user._id)
+    .then(ui.onGetCartSuccess)
+    .catch(console.error)
+}
+
+const onCreateCart = id => {
+  console.log('create cart')
+  api.createCart(id)
+    .then(console.log)
+    .catch()
+}
+
+const onAddCart = event => {
+  // console.log(event)
+  // const productId = $(event.target).closest('div.product').data('id')
+  // console.log(productId)
+  const productId = event.target.dataset.id
+  // console.log(productId)
+  // console.log('cart in add', store.cart)
+  const cart = store.cart
+  // console.log(cart)
+  const productAddToCart = {
+    product: productId,
+    quantity: 1
+  }
+
+  store.Found = false
+
+  // prevent repeat push product into cart
+  for (let i = 0; i < cart.products.length; i++) {
+    const products = cart.products[i]
+    if (products.product._id === productId) {
+      products.quantity = products.quantity + 1
+      store.Found = true
+    }
+  }
+  if (!store.Found) {
+    cart.products.push(productAddToCart)
+  }
+
+  console.log('sum is', store.Sum)
+  const data = {}
+  data.cart = cart
+  console.log('data', data)
+  api.updateCart(data)
+    .then(onGetCart)
+    .then(console.log())
+    .catch()
+}
+
+const onRemoveProduct = event => {
+  const productId = event.target.dataset.id
+  console.log(productId)
+  const cart = store.cart
+  for (let i = 0; i < cart.products.length; i++) {
+    const products = cart.products[i]
+    if (products.product._id === productId) {
+      products.quantity = products.quantity - 1
+    }
+  }
+  cart.products = cart.products.filter((product) => product.quantity > 0)
+
+  const data = {}
+  data.cart = cart
+  console.log('data', data)
+  api.updateCart(data)
+    .then(onGetCart)
+    .then(console.log())
+    .catch()
+}
 
 const addCartEventHandlers = () => {
-
+  $('body').on('click', '.add-cart', onAddCart)
+  $('#view-cart').on('click', onGetCart)
+  $('body').on('click', '.addCart', onAddCart)
+  $('body').on('click', '.subtractionCart', onRemoveProduct)
 }
 
 module.exports = {
-  addCartEventHandlers
+  addCartEventHandlers,
+  onCreateCart,
+  onGetCart
 }
