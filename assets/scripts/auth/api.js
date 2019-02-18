837aa84ed105c949bdd7cd8e12273cd5ab435409
xiaojie zhang <xiaojiezhang@xiaojiedeMacBook-Air.local>
2019-02-18 13:32:44 -0500

    Add auth -add sign-in sign-up change-password sign-out function in events -add ajax request to four user action in api -add success and failure function for each user action in ui -test successful

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,44 @@
+const config = require('../config')
+const store = require('../store')
+
+const signIn = data => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data
+  })
+}
+const signUp = data => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data
+  })
+}
+const signOut = () => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const changePassword = data => {
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+module.exports = {
+  signIn,
+  signUp,
+  signOut,
+  changePassword
+}
