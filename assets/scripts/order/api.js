ea651a69b446379932587a2632c08ce2c05fa031
JennasaurusRex <jlnorthcott@gmail.com>
2019-02-19 11:17:19 -0500

    Integrate Stripe Checkout to client
    - Download Stripe package with npm install.
    - Add stripe script location in index.html.
    - Create API call to stripe when checkout button is clicked.
    - Add success or failure ui message.

--- assets/scripts/order/api.js
+++ assets/scripts/order/api.js
@@ -1,1 +1,23 @@
 'use strict'
+<<<<<<< 6dbb5f8abe6117d605eb3a8ea6d023a77cdb1721
+=======
+
+const config = require('../config')
+const store = require('../store')
+
+const checkout = (token) => {
+  console.log(token)
+  return $.ajax({
+    url: config.apiUrl + '/charge',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: {'token': token, 'amount': parseInt(store.Sum * 100)}
+  })
+}
+
+module.exports = {
+  checkout
+}
+>>>>>>> Integrate Stripe Checkout to client
--- assets/scripts/order/ui.js
+++ assets/scripts/order/ui.js
@@ -1,1 +1,17 @@
 'use strict'
+<<<<<<< 6dbb5f8abe6117d605eb3a8ea6d023a77cdb1721
+=======
+
+const onCheckoutSuccess = () => {
+  $('#user-message').html('<a class="btn btn-danger">Check out success</a>')
+}
+
+const onCheckoutFailure = () => {
+  $('#user-message').html('<a class="btn btn-danger">Check out failed.</a>')
+}
+
+module.exports = {
+  onCheckoutSuccess,
+  onCheckoutFailure
+}
+>>>>>>> Integrate Stripe Checkout to client
