5437f0cbd9dfa505be25777adfba7dcf75d3d2d1
JennasaurusRex <jlnorthcott@gmail.com>
2019-02-19 11:17:19 -0500

    Integrate Stripe Checkout to client
    - Download Stripe package with npm install.
    - Add stripe script location in index.html.
    - Create API call to stripe when checkout button is clicked.
    - Add success or failure ui message.

--- assets/scripts/order/events.js
+++ assets/scripts/order/events.js
@@ -2,11 +2,43 @@
 
 const api = require('./api')
 const ui = require('./ui')
+const store = require('../store')
+
+const checkoutHandler = StripeCheckout.configure({
+  key: 'pk_test_Ba2NFx0UbzDjWo1LB87WJXYN',
+  locale: 'auto'
+})
+
+// const button = document.getElementById('checkout')
+// button.addEventListener('click', function (ev) {
+//   checkoutHandler.open({
+//     name: 'Sample Store',
+//     description: 'Example Purchase',
+//     token: handleToken
+//   })
+// })
+
+const handleToken = function (token) {
+  console.log(store.Sum)
+  api.checkout(token)
+    .then(ui.onCheckoutSuccess)
+    .catch(ui.onCheckoutFailure)
+}
+
+const onCheckout = () => {
+  checkoutHandler.open({
+    name: 'Nozama',
+    description: 'Purchase',
+    token: handleToken,
+    amount: store.Sum * 100
+  })
+}
 
 const addOrderEventHandlers = () => {
-
+  $('body').on('click', '#checkout', onCheckout)
 }
 
 module.exports = {
+  checkoutHandler,
   addOrderEventHandlers
 }
